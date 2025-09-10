fcedfa9b25e547302a196fd1a49f480ea3349afa
Hyugai1032 <rosaleskaentkraven@gmail.com>
2025-09-10 15:54:59 +0800

    Second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '10ec009a45eb14b8f57c23e755b1e0857c08ac69',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '10ec009a45eb14b8f57c23e755b1e0857c08ac69',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
