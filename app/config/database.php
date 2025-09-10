4128035c791b30501028f0045dd6b6dcfb87870f
Hyugai1032 <rosaleskaentkraven@gmail.com>
2025-09-10 16:45:08 +0800

    Latest commmit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv("sql12.freesqldatabase.com"),
-    'port'		=> getenv("3306"),
-    'username'	=> getenv("sql12798030"),
-    'password'	=> getenv("d5bUPrGYdQ"),
-    'database'	=> getenv("sql12798030"),
+    'hostname'	=> getenv("DB_HOST"),
+    'port'		=> getenv("DB_PORT"),
+    'username'	=> getenv("DB_USER"),
+    'password'	=> getenv("DB_PASS"),
+    'database'	=> getenv("DB_NAME"),
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
