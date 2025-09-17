5f23cc4145fc281900b839d168f421837d54e8cc
Hyugai1032 <rosaleskaentkraven@gmail.com>
2025-09-17 16:42:36 +0800

    Pagination

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller {
 
         $records_per_page = 10;
 
-        $all = $this->author_model->page($q, $records_per_page, $page);
+        $all = $this->UserModel->page($q, $records_per_page, $page);
         $users['all'] = $all['records'];
         $total_rows = $all['total_rows'];
         $this->pagination->set_options([
